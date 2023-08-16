72b53d369eedaa12c3b646be11a6c2c578b745bf
John Doe <johndoe@email.com>
2023-08-16 16:00:53 +0500

    исправила тест

--- testing.test.ts
+++ testing.test.ts
@@ -1,16 +1,16 @@
 import { test, expect, describe } from "@jest/globals";
 const { arrCardsImages } = require("./components/arrCardsImages");
 const { arrCardsFlip } = require("./components/renderGameLevel");
+const {arrCardsFlipDuplicate} = require('./components/renderGameLevel');
+const { shuffle } = require("lodash");
 
 describe("проверки", () => {
   test("есть ли в массиве элементы", () => {
     expect(arrCardsFlip).not.toBeNull();
   });
 
-  test("есть ли картинка с определенным путём", () => {
-    expect(arrCardsFlip).toContain(
-      `<img id="cards-click" data-index="${i}" class="cards-suits" src="../static/img/рубашка.png">`,
-    );
+  test("валиден ли массив с элементами", () => {
+    expect(shuffle(arrCardsFlipDuplicate)).not.toContain(true);
   });
 
   test("длина массива 36", () => {
